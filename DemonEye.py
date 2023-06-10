c795ee666d7f64e41993c18a7ec26b3ba422b97e
root <root@kali>
2023-06-10 13:24:04 +0500

    V2
    Added Wifi Adapter Selection Option And Fixed Some Bugs

--- DemonEye.py
+++ DemonEye.py
@@ -1,3 +1,4 @@
+import subprocess
 import os
 
 print ('''
@@ -18,6 +19,51 @@ _MMMMMMM9' _MMMMMMMMM _M_      _MM_  YMMMM9   _M_      \M       _MMMMMMMMM     _
                                                                                                    
                                                                                                    
 ''')
-iface = input ('Enter Your Wifi Interface Name : ')
+def loop():
+    global iface
+    def get_wifi_adapters():
+        try:
+            output = subprocess.check_output(['iw', 'dev'], stderr=subprocess.STDOUT, universal_newlines=True)
+            lines = output.split('\n')
+            adapters = []
+
+            for line in lines:
+                if 'Interface' in line:
+                    adapter = line.split(' ')[-1]
+                    adapters.append(adapter)
+
+            return adapters
+        except subprocess.CalledProcessError as e:
+            print(f"Command execution failed with error: {e.output}")
+
+    wifi_adapters = get_wifi_adapters()
+
+    if len(wifi_adapters) > 0:
+        print("Available Wi-Fi adapters:\n")
+        for index, adapter in enumerate(wifi_adapters, start=1):
+            print(f"{index}. {adapter}\n")
+
+        while True:
+            selection = input("Enter the number corresponding to the Wi-Fi adapter you want to select: \n\n")
+            
+            try:
+                selection = int(selection)
+                if selection >= 1 and selection <= len(wifi_adapters):
+                    iface = wifi_adapters[selection - 1]
+                    print(f"Selected Wi-Fi adapter: {iface}")
+                    break
+                else:
+                    print("\nInvalid selection.\n")
+            except ValueError:
+                print("\nInvalid input. Please enter a number.\n")
+    else:
+        print("No Wi-Fi adapters found.")
+
+loop()
+
+os.system ('airmon-ng check kill')
+os.system (f'airmon-ng start {iface}')
 print ('\nProbe Capturing Started. \nPress Ctrl+C To Stop\n')
 os.system (f'tcpdump -l -i {iface} -e -s 256 type mgt subtype probe-req | awk -f tcpdump.awk | tee -a "captured_probes.txt" ')
+os.system (f'airmon-ng stop {iface}')
+os.system ('service NetworkManager restart')
